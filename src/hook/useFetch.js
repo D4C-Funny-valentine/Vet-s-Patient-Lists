57909ee2c47bef685f07e7caf000d98abaf6fd07
D4C-Funny-valentine <kyawn5194@gmail.com>
2023-12-31 10:09:22 +0630

    updated

--- src/hook/useFetch.js
+++
@@ -1,9 +0,0 @@
-import { baseUrl } from "@/helper/baseUrl";
-
-const useFetch = async () => {
-  const res = await fetch(`${baseUrl}/patients`, { cache: "no-store" });
-  const data = await res.json();
-  return data;
-};
-
-export default useFetch;
